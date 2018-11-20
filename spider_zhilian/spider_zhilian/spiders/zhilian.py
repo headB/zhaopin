b1a602e8fa6464fdef6a629da04ad32bcdbeeb52
headB_macbook_pro <775121173@qq.com>
2018-11-20 10:34:43 +0800

    starts_urls有几条,这个爬虫就爬几个url地址.

--- spider_zhilian/spider_zhilian/spiders/zhilian.py
+++ spider_zhilian/spider_zhilian/spiders/zhilian.py
@@ -1,7 +1,28 @@
 #写一个爬虫尝试爬取智联招聘
 
 from scrapy import Spider
+#添加去重
+# from scrapy.dupefilters import RFPDupeFilter
+
+
+#就是,爬虫教程,有提及的,就是,最基本的爬虫,包括了
+#item
+#name
+#start_urls
+#parse(这个是后续的处理吧)
 
 class zhiLianSpider(Spider):
     name = "zhilian_1"
-    
+    allowed_domains = ['zhaopin.com']
+    start_urls = [
+        'https://sou.zhaopin.com/?jl=489',
+    ]
+
+    def parse(self,response):
+
+        pass
+        print("捉到我一次!")
+        print(response)
+
+        #然后就可以写规则了.
+        
