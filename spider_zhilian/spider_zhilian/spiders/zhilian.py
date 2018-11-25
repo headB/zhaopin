c84da8d7639d4d2e509fdd2f39e8483725ee010e
headB_macbook_pro <775121173@qq.com>
2018-11-26 04:17:52 +0800

    自带的scrapy_redis分布式也可以适应自己的要求爬取数据了,然后也证明了Rule好像在scrapy_redis里面真的没有什么卵用.

--- spider_zhilian/spider_zhilian/spiders/zhilian.py
+++ spider_zhilian/spider_zhilian/spiders/zhilian.py
@@ -27,12 +27,14 @@ from scrapy_redis.spiders import RedisSpider
 
 class zhiLianSpider(RedisSpider):
     name = "zhilian_1"
+    start_urls = ''
 
     def __init__(self):
         self.allowed_domains = ['zhaopin.com']
-        start_urls = [
-            'https://sou.zhaopin.com/?jl=489',
-        ]
+        self.start_urls = 'https://sou.zhaopin.com/?jl=489'
+        # for x in self.create_2_request("123"):
+        #     pass
+        
     
     rules = (
         Rule(LinkExtractor(allow=(r"https://fe-api.zhaopin.com/c/i/sou/\?cityId")),callback='handle_json_2_item',follow=True),
@@ -42,10 +44,8 @@ class zhiLianSpider(RedisSpider):
 
     #redisSpider说,一定需要定义好parse函数,那没办法了,那就定义吧.
     def parse(self,response):
-        pass
-        for x in self.create_2_request(response):
-
-            pass
+        
+        yield self.handle_json_2_item(response)
         
 
 
@@ -68,12 +68,16 @@ class zhiLianSpider(RedisSpider):
             items['experience'] = x['workingExp']['name']
             items['salary'] = x['salary']
             items['job_name'] = x['jobName']
-            yield items
+        return  items
 
     #创建批量的请求,
     #这里的话,还需要解析出具体的省份信息
+
+    #此处,应该只能运行一次!因为只是负责生成批量的url地址.!
+    #对了,就是直接将这方法扔到init的地方就可以完成初始化了.!
     def create_2_request(self,response):
         #那需要获取省份信息的话,那就调用旁边的啦!.哈哈.提高利用率嘛.
+        
         
         #这里就先假设是用热门城市来做了
         hot_city = return_province_info()['hot_citys']
@@ -81,11 +85,14 @@ class zhiLianSpider(RedisSpider):
         #制作大量url地址,又重新提交给scrapy
         #然后就是code和url配合
         redis_conn = manu_conn_redis1
+
+        redis_conn.lpush("zhilian_1:start_urls",self.start_urls)
+
         for x in hot_city:
             for x1 in crawler_zhilian(x['code']):
                 for x2 in x1:
                     print(x2)
-                    # yield redis_conn.lpush("zhilian_1:start_urls",x2)
+                    yield redis_conn.lpush("zhilian_1:start_urls",x2)
                     # yield scrapy.Request(x2,callback=self.handle_json_2_item)
 
 
